df5691c55a4e5e46e1e74799eb393f91e66b1c5e
Gustavo "Goose" Grau Bolivar <gustavo.graubolivar@kellogg.northwestern.edu>
2023-04-21 21:54:23 +0000

    Committing changes to poker dealer basic.

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -18,12 +18,55 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 # ...
 
 for suit in suits
+    puts "____#{suit} ____"
     for rank in ranks
         puts "#{rank} #{suit}"
     end
 end
+puts "________________________"
 
 # CHALLENGE
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
 
+# 1. Creating and filling in the deck of cards
+
+deck = []
+
+for suit in suits
+    for rank in ranks
+        rank = rank.to_s
+        suit = suit.to_s
+        card = rank+" "+suit
+        deck.push(card)
+    end
+end
+
+# 1a. Corroborating there's 52 cards
+
+ if deck.length == 52 
+    puts "Deck created!"
+ end
+
+ #2. Define variables (i.e., number of players and cards drawn)
+
+ #3. Draw 5 random cards from the deck:
+
+ hand = []
+  
+ for i in (0..5)
+    index = rand(0..51)
+    draw = deck[index]
+    hand.push(draw)
+end
+
+puts " "
+puts "Dealing... "
+puts " "
+puts "Here's your hand"
+puts "_______________"
+for card in hand   
+    puts "#{card}"
+end
+
+# EXTRA_CHALLENGE 
